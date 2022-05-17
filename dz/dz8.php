075bb95fe414fa1a30849d07a49d2bb089845fbb
Kotislav <riotwf111@gmail.com>
2022-05-17 21:26:23 +0300

    lol

--- dz/dz8.php
+++ dz/dz8.php
@@ -11,5 +11,19 @@ if ($year % 400 === 0) {
     $result= ' default year';
 }
 echo $result;
+///
+///
+///Добре. А тепер спробуйте об'єднати два if за допомогою логічних операторів.
+///
+/// Типа такого? не знаю почему сразу так не сделал
+$year = random_int(0, 9999);;
+echo $year;
+$result = '';
+if ($year % 400 === 0||($year % 100 !== 0 && $year % 4 === 0)){
+    $result= ' leap year';}
+ else {
+    $result= ' default year';
+}
+echo $result;
 
 
