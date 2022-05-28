b8522b9ad5b036e1be6bdad2cf2980d4dbaaffed
Kotislav <riotwf111@gmail.com>
2022-05-28 15:57:40 +0300

    news

--- dz/dz5.php
+++ dz/dz5.php
@@ -2,3 +2,17 @@
 $number = random_int(1000, 9999);
 $result = array_sum(str_split($number));
 echo "Сумма чисел числа $number = $result" , PHP_EOL;
+//Чую запах Stack Overflow.
+//
+//А в цілому ви не дотримались умови задачі.
+// Там сказано знайти суму цифр за допомогою арифметичних операцій. Використовуйте / та %
+
+
+$number = random_int(1000, 9999);
+$a = $number % 10;
+$b = ($number / 10) % 10;
+$c = ($number / 100) % 10;
+$d = ($number / 1000) % 10;
+echo PHP_EOL;
+$varSum = $a + $b + $c + $d;
+echo $number, PHP_EOL, $varSum;
